29598f4beff753f1d070c7944f392dc47584e43e
Alex Meña <alex.mena@stationfive.com>
2024-12-25 21:15:21 +0800

    modify layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,28 +55,28 @@ export default function Home() {
         <SignIn setUser={setUser} />
       ) : (
         <div className="container mx-auto">
-          <div className="grid grid-cols-12 gap-2">
-            <div className="col-span-8">
-              <div className="flex flex-col h-screen">
-                <div className="rounded border min-h-4 p-2 flex-1 overflow-auto">
-                  {messages.map((msg, i) => (
-                    <div key={i}>{msg.message}</div>
-                  ))}
-                  <div ref={scroller} />
-                </div>
-                <div className="">
-                  <form onSubmit={handleSubmit} className="flex gap-2 my-1">
-                    <Input
-                      type="text"
-                      onChange={(e) => setInput(e.target.value)}
-                      value={input}
-                    />
-                    <Button type="submit" disabled={input.length < 1}>
-                      Send
-                    </Button>
-                  </form>
-                </div>
-              </div>
+          <div className=" h-[100vh] flex flex-col gap-2 px-4" id="wrapper">
+            <div id="header" className=" mt-5">
+              <h1 className="text-2xl font-bold">Chatroom</h1>
+            </div>
+            <div className="rounded border flex-auto p-2 min-h-8 overflow-auto">
+              {messages.map((msg, i) => (
+                <div key={i}>{msg.message}</div>
+              ))}
+              <div ref={scroller} />
+            </div>
+
+            <div id="footer" className=" mb-5">
+              <form onSubmit={handleSubmit} className="flex gap-2 my-1">
+                <Input
+                  type="text"
+                  onChange={(e) => setInput(e.target.value)}
+                  value={input}
+                />
+                <Button type="submit" disabled={input.length < 1}>
+                  Send
+                </Button>
+              </form>
             </div>
           </div>
         </div>
